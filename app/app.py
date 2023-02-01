c10a70b8499a47b1be1402938167c6130ce695d2
Norman Mark (ETAS/ESW6) <mark.norman@etas.com>
2023-02-01 11:57:27 +0000

    Exercise from Wed 25 Jan 2023

--- app/app.py
+++ app/app.py
@@ -1,12 +1,47 @@
 from flask import Flask, render_template, request, redirect, url_for
 
+import requests
+import os
+import random
+
+nasa_planet_url=r'https://api.nasa.gov/planetary/apod'
+nasa_rovers_url=r'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos'
+
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', landing_image='https://via.placeholder.com/500')
+
+    query={'api_key': os.getenv('API_KEY')  }
+
+    headers = {"Accept": "application/json"}
+
+    response = requests.request(
+    "GET",
+    nasa_planet_url,
+    headers=headers,
+    params=query,
+    verify=False
+    )
+    return render_template('index.html', landing_image=response.json()['url'])
 
 @app.route('/mars')
 def mars():
-    return render_template('mars.html')
+
+    query={'sol': 1000, 'api_key': os.getenv('API_KEY')  }
+
+    headers = {"Accept": "application/json"}
+
+    response = requests.request(
+    "GET",
+    nasa_rovers_url,
+    headers=headers,
+    params=query,
+    verify=False
+    )
+
+
+    return render_template('mars.html', mars_img_list=[entry['img_src'] for entry in response.json()['photos']])
